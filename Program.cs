faa160774ba0b8c8987f397ef74ad7950264c916
Sergo331 <proha331@gmail.com>
2023-01-28 21:39:52 +0300

    магазин кристаллов
    магазин кристаллов

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            int priceCrystal = 10;
+            int crystalCount;
+
+            Console.WriteLine("Добро пожаловать в магазин кристаллов. Цена 1 кристалла " + priceCrystal + " монет.");
+            Console.Write("Сколько у вас монет?:");
+            money = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Сколько кристаллов Вам нужно?:");
+            crystalCount = Convert.ToInt32(Console.ReadLine()); 
+
+            money -= priceCrystal * crystalCount;
+
+            Console.WriteLine("У вас в сумке: " + crystalCount + " кристаллов и " +money +" монет.");
+        }
+    }
+}
